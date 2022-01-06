48aa6d6a2da81ac6e87df458c38a432146955b22
BatcheDaka <dakata@gmail.com>
2022-01-06 20:10:42 +0200

    searchbar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,13 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 import Results from './Results';
-
+import Pagination from './Pagination';
 function SearchBar() {
   
     const [searchInput, setSearchInput] = useState('');
     const [repos, setRepos] = useState(JSON.parse(localStorage.getItem('prevRepo')) || []);
-    
-    
+    const [currentPage, setCurrentPages] = useState(1);
+    const [postsPerPage] = useState(10);
 
       const handleChange = (e) => {
           setSearchInput(e.target.value);
@@ -24,23 +24,40 @@ function SearchBar() {
         localStorage.removeItem("prevRepo")
     
         try {
-const result = await axios(`https://api.github.com/repos/${searchInput}/issues`);
+const result = await axios(`https://api.github.com/repos/${searchInput}/issues?per_page=100&page=${currentPage}`);
 setRepos(result);
-console.log(result);
+
 localStorage.setItem("prevRepo",  JSON.stringify(result))
         } catch(err){
             setRepos([]);
         }
 
       };
-   
+    
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+   let currentPosts = repos.data;
+   let length = 0;
+  if (currentPosts) {
+    length = currentPosts.length;
+   currentPosts=  currentPosts.slice(indexOfFirstPost, indexOfLastPost)
+    
+  } 
+//   else {
+//       currentPosts = [];
+//   }
+console.log(currentPosts);
+
+const paginate = (pageNumber) => setCurrentPages(pageNumber);
   return (
     <> 
     <div style={{padding: "20px"}}>
    <input type="text" placeholder="Search" value={searchInput} onChange={handleChange}/>
    <button onClick={handleClick}>Search</button>
     </div>
-    <Results repos={repos}/>
+    <Results repos={currentPosts || []}/>
+    <Pagination postsPerPage={postsPerPage} totalPosts={length}
+     paginate={paginate}/>
     </> 
   );
 }
